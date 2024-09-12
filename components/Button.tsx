fa92f2b0977245aea0a6e2181fbe42f81aa5a42f
Tpham004 <thomaspham.exec@gmail.com>
2024-09-12 03:12:40 -0400

    Working on Button

---
+++ components/Button.tsx
@@ -0,0 +1,22 @@
+import { forwardRef } from "react";
+
+interface ButtonProps
+    extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
+
+const Button = forwardRef<HTMLButtonElement, ButtonProps>(({
+    className,
+    children,
+    disabled,
+    type = "button",
+    ...props
+}, ref) => {
+ return {
+     <button>
+
+     </button>
+  }
+})
+
+Button.displayName = "Button";
+
+export default Button;
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,11 @@
-"use client"
+"use client";
 
 import { useRouter } from "next/navigation";
+import { RxCaretLeft, RxCaretRight } from "react-icons/rx";
+import { twMerge } from "tailwind-merge";
+import { HiHome } from "react-icons/hi";
+import { BiSearch } from "react-icons/bi";
+import Button from "./Button";
 
 
 interface HeaderProps {
@@ -20,10 +25,107 @@ const Header: React.FC<HeaderProps> = ({
   }
 
   return (
-    <div>
-     
+    <div
+      className={twMerge(`
+          h-fit
+          bg-gradient-to-b
+          from-emerald-800
+          p-6
+        `,
+          className
+      )}
+    >
+     <div className="
+       w-full
+       mb-4 
+       flex
+       items-center
+       justify-between
+     ">
+      <div className="
+        hidden 
+        md:flex
+        gap-x-2
+        items-center
+      ">
+        <button
+          onClick={() => router.back()}
+          className="
+            rounded-full
+            bg-black
+            flex
+            items-center
+            justify-center
+            hover:opacity-75
+            transition
+          "
+        >
+          <RxCaretLeft className="text-white" size={35} />
+        </button>
+        <button
+          onClick={() => router.forward()}
+          className="
+            rounded-full
+            bg-black
+            flex
+            items-center
+            justify-center
+            hover:opacity-75
+            transition
+          "
+        >
+          <RxCaretRight className="text-white" size={35} />
+        </button>
+      </div>
+      <div className="flex md:hidden gap-x-2 items-center">
+        <button
+          className="
+            rounded-full
+            p-2
+            bg-white
+            flex
+            items-center
+            justify-center
+            hover:opacity-75
+            transition
+          "
+        >
+          <HiHome className="text-black" size={20}/>
+        </button>
+        <button
+          className="
+            rounded-full
+            p-2
+            bg-white
+            flex
+            items-center
+            justify-center
+            hover:opacity-75
+            transition
+          "
+        >
+          <BiSearch className="text-black" size={20}/>
+        </button>
+      </div>
+      <div
+        className="
+          flex
+          justify-between
+          items-center
+          gap-x-4
+        "
+      >
+        <>
+          <div>
+            <Button>
+              Sign up
+            </Button>
+          </div>
+        </>
+      </div>
+     </div>
     </div>
-  )
-};
+  );
+}
 
 export default Header;
