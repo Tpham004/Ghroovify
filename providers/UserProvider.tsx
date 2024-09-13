669532a80692a082c1361c9c3adb7bd74f55f971
Tpham004 <thomaspham.exec@gmail.com>
2024-09-12 21:44:05 -0400

    Finished Providers for Authentication

---
+++ providers/UserProvider.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { MyUserContextProvider } from "@/hooks/useUser";
+
+interface UserProviderProps {
+    children: React.ReactNode;
+};
+
+const UserProvider: React.FC<userProviderProps> = ({
+    children
+}) => {
+    return (
+        <MyUserContextProvider>
+            {children}
+        </MyUserContextProvider>
+    )
+};
+
+export default UserProvider;
