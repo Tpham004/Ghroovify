50042e65b351c65ad03fa3d5462d47e1d8dbab64
Tpham004 <thomaspham.exec@gmail.com>
2024-09-15 10:35:12 -0400

    Started Song Fetching/List Display

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,9 +1,13 @@
+import getSongs from "@/actions/getSongs";
 import Header from "@/components/Header"
 import ListItem from "@/components/ListItem"
+import PageContent from "./components/PageContent";
 
 export const revalidate = 0;
 
-export default function Home() {
+export default async function Home() {
+  const songs = await getSongs();
+
   return (
    <div className="
     bg-neutral-900
@@ -50,9 +54,7 @@ export default function Home() {
           Newest songs
         </h1>
       </div>
-      <div>
-        List of Songs!
-      </div>
+      <PageContent songs={songs} />
     </div>
    </div>
   )
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        domains: [
+            "elmaeosjortlmzviejmk.supabase.co"
+        ]
+    }
+};
 
 export default nextConfig;
