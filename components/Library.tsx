eb6cd9e616ff5a61dd9086bfa2bc914174237bf6
Tpham004 <thomaspham.exec@gmail.com>
2024-10-09 22:40:43 -0400

    Final Push

--- components/Library.tsx
+++ components/Library.tsx
@@ -1,95 +1,56 @@
-"use client";
+import { TbPlaylist } from "react-icons/tb"
+import { AiOutlinePlus } from "react-icons/ai"
 
-import { TbPlaylist } from "react-icons/tb";
-import { AiOutlinePlus } from "react-icons/ai";
-import useAuthModal from "@/hooks/useAuthModal";
-import { useUser } from "@/hooks/useUser";
-import useUploadModal from "@/hooks/useUploadModal";
-import { Song } from "@/types";
-import MediaItem from "./MediaItem";
-import useOnPlay from "@/hooks/useOnPlay";
+import useAuthModal from "@/hooks/useAuthModal"
+import { useUser } from "@/hooks/useUser"
+import useUploadModal from "@/hooks/useUploadModal"
+import { Song } from "@/types"
+import MediaItem from "./MediaItem"
+import useOnPlay from "@/hooks/useOnPlay"
+import useSubscribeModal from "@/hooks/useSubscribeModal"
 
 interface LibraryProps {
-  songs: Song[];
+  songs: Song[]
 }
 
-const Library: React.FC<LibraryProps> = ({
-  songs
-}) => {
-  const authModal = useAuthModal();
-  const uploadModal = useUploadModal();
-  const { user } = useUser();
+const Library = ({ songs }: LibraryProps) => {
+  const subscribeModal = useSubscribeModal()
+  const authModal = useAuthModal()
+  const uploadModal = useUploadModal()
+  const { user, subscription } = useUser()
 
-  const onPlay = useOnPlay(songs);
-
+  const onPlay = useOnPlay(songs)
 
   const onClick = () => {
     if (!user) {
-      return authModal.onOpen();
+      return authModal.onOpen()
     }
-
-    //todo check for subscription
-
-    return uploadModal.onOpen();
-  };
+    if (!subscription) {
+      return subscribeModal.onOpen()
+    }
+    return uploadModal.onOpen()
+  }
 
   return (
-    <div className="flex flex-col">
-        <div
-          className="
-            flex
-            items-center
-            justify-between
-            px-5
-            pt-4
-          "
-        >
-            <div
-              className="
-                inline-flex
-                items-center
-                gap-x-2
-              "
-            >
-                <TbPlaylist className="text-neutral-400" size={26}/>
-                <p
-                  className="
-                    text-neutral-400
-                    font-medium
-                    text-md
-                  "
-                >
-                    Your Library
-                </p>
-            </div>
-            <AiOutlinePlus 
-                onClick={onClick}
-                size={20}
-                className="
-                    text-neutral-400
-                    cursor-pointer
-                    hover:text-white
-                    transition
-                "
-            />
+    <div className="flex flex-col ">
+      <div className="flex items-center justify-between px-5 py-4">
+        <div className="inline-flex items-center gap-2">
+          <TbPlaylist className="text-neutral-400" size={26} />
+          <p className="text-neutral-400 font-medium text-md">Your libray</p>
         </div>
-        <div className="
-            flex
-            flex-col
-            gap-y-2
-            mt-4
-            px-3
-        ">
-            {songs.map((item) => (
-              <MediaItem 
-                onClick={(id: string) => onPlay(id)}
-                key={item.id}
-                data={item}
-              />
-            ))}
-        </div>
+        <AiOutlinePlus
+          className="text-neutral-400 cursor-pointer hover:text-white transition"
+          onClick={onClick}
+          size={20}
+        />
+      </div>
+      <div className="flex flex-col gap-y-2 mt-4 px-3">
+        {songs.map(song => (
+          <MediaItem onClick={(id: string) => onPlay(id)} key={song.id} data={song} />
+        ))}
+      </div>
     </div>
-  );
+  )
 }
 
-export default Library;
+export default Library
